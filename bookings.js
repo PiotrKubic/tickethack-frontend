06bc3db7cf11b2807dd962d50fa91417a2944802
Simon Bucki <bucki.simon@gmail.com>
2024-09-25 14:54:35 +0200

    finished booking html + logic

--- bookings.js
+++ bookings.js
@@ -5,35 +5,51 @@ getBookings();
 async function getBookings() {
   const bookingsResponse = await fetch(`${backendURL}/bookings/`);
   const bookings = await bookingsResponse.json();
-  displayBookings(bookings.bookings);
+  if ((bookings.length = 0)) {
+    return;
+  } else {
+    displayBookings(bookings.bookings);
+  }
 }
 
 function displayBookings(bookings) {
-  bookings = bookings.map((e) => e.tripId);
-  const resultContainer = document.querySelector("#result-container");
+  bookings = bookings
+    .map((e) => e.tripId)
+    .sort((a, b) => moment.utc(a.date) - moment.utc(b.date));
+  const bookingContainer = document.querySelector("#bookings-container");
+  const myBookings = document.querySelector("#my-bookings-title");
   const noBookings = document.querySelector("#no-bookings");
   const planMessage = document.querySelector("#plan-message");
+  const separator = document.querySelector("#separator");
+  const enjoyMessage = document.querySelector("#enjoy-message");
+  noBookings.style.display = "none";
+  planMessage.style.display = "none";
+  myBookings.style.display = "block";
+  enjoyMessage.style.display = "block";
+  separator.style.display = "block";
   for (let booking of bookings) {
     const time = moment.utc(booking.date).format("HH:mm");
     const timeUntilDeparture = getTimeUntilDeparture(booking.date);
-    noBookings.style.display = "none";
-    planMessage.style.display = "none";
-    resultContainer.innerHTML += `<div class="booking-container" id="${booking._id}">
+    bookingContainer.innerHTML += `<div class="booking-container" id="${booking._id}">
   <div class="trip">${booking.departure} > ${booking.arrival}</div>
   <div class="time">${time}</div>
   <div class="price">${booking.price}€</div>
+  <div class="timeUntilDeparture">${timeUntilDeparture}</div>
   </div>`;
   }
 }
 
 function getTimeUntilDeparture(dateTime) {
   const timeNow = moment.utc();
-  console.log(timeNow);
   const departureTime = moment.utc(dateTime);
-  console.log(departureTime);
-  const timeUntilDeparture = departureTime - timeNow;
-  console.log(timeUntilDeparture);
+  const timeUntilDeparture = Math.round((departureTime - timeNow) / 1000 / 60);
   if (timeUntilDeparture < 0) {
     return "Departed";
+  } else if (timeUntilDeparture < 60) {
+    return `Departure in ${timeUntilDeparture} minutes`;
+  } else if (timeUntilDeparture < 24 * 60) {
+    return `Departure in ${Math.round(timeUntilDeparture / 60)} hours`;
+  } else {
+    return `Departure in ${Math.round(timeUntilDeparture / 60 / 24)} days`;
   }
 }
